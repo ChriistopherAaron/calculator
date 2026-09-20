538d1c8f58f9a0133587fb1f6c53fb338365efde
AaronTooNice <256012394+ChriistopherAaron@users.noreply.github.com>
2026-09-20 00:05:20 -0700

    Add basic math functions and operate function

--- script.js
+++ script.js
@@ -15,3 +15,18 @@ const divide = function(num1, num2) {
     return num1 / num2;
     }
 };
+let firstNumber = "";
+let operator = "";
+let secondNumber = "";
+
+const operate = function(operator, num1, num2) {
+if (operator === '+') {
+  return add(num1, num2);
+} else if (operator === '-') {
+  return subtract(num1, num2);
+} else if (operator === '*') {
+  return multiply(num1, num2);
+} else if (operator === '/') {
+  return divide(num1, num2);
+}
+};

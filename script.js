b98089bb8d37c828f0d78b469e28073601b60356
AaronTooNice <256012394+ChriistopherAaron@users.noreply.github.com>
2026-09-19 23:49:43 -0700

    Added basic functions to script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+/* Starting with adding in basic functions */
+const add = function(num1, num2) {
+    return num1 + num2;
+} 
+const subtract = function(num1, num2) {
+    return num1 - num2;
+}
+const multiply = function(num1, num2) {
+  return num1 * num2 ;
+}
+const divide = function(num1, num2) {
+      if (num2 === 0) {
+        return "OOPS";
+    } else {
+    return num1 / num2;
+    }
+};
